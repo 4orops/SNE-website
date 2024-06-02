b0fc9b594ac848474425617f12aeb328bdded91b
4orops <wabile4@gmail.com>
2024-06-02 16:34:16 +0200

    bootstrappi

--- client/src/components/home3.js
+++ client/src/components/home3.js
@@ -3,7 +3,7 @@ import React from "react";
 function Home3() {
     return (
         <div className="container">  
-            <div className="col-12">
+            <div className="col-12">    
                 <div className="title-wrapper">
                     <h2 className="mbr-section-title mbr-fonts-style display-2">
                         <strong>News / Articles </strong>
