205b6a5c404f2f8ffd3bf6dffe6003eeab968e43
4orops <wabile4@gmail.com>
2024-06-03 17:38:05 +0200

    adding contact form

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -4,19 +4,24 @@ import { Container, Row, Col } from "react-bootstrap";
   
   function Contact() {
     return (
-      <section>
-        <Container fluid className="home-section" id="home">
-          <Container  className="home-content">
-            <Row>
-              <Col md={7} className="home-header">
-                <h1 className="heading-name">
-                  This the Contact Page
-                </h1>             
-              </Col>          
-            </Row>  
-          </Container>
-        </Container>
-      </section>
+      <div className="container">
+        <h1 className="display-2 p-5"><strong> Contact Us </strong></h1>
+        <div className="col bg-secondary">
+        <form>
+          <div className="mb-3">
+          <label className="form-label"> First Name </label>
+          <input type="text"  className="form-control" placeholder="Enter Your Name"></input>
+          <label className="form-label"> Last Name </label>
+          <input type="text" className="form-control" placeholder="Enter Second Name"></input>
+          <label className="form-label"> Email Address </label>
+          <input type="email" className="form-control" placeholder="example@email.com"></input>
+          <label className="form-label"> Your Message </label>
+          <textarea className="form-control" rows="4" cols="50"></textarea>
+          <input type="submit" value="Submit" className="btn btn-success mt-3"></input>
+          </div>
+        </form>
+        </div>
+      </div>
     );
   }
   
